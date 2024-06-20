715f5edde504cfbb4da17f461ac05ba8ca2dada6
Roger Lo <sobadrush@icloud.com>
2024-06-20 23:43:06 +0800

    修改 touchScreen.py

--- 3_touchscreen.py
+++ 3_touchscreen.py
@@ -20,7 +20,7 @@ while True:
     time.sleep(0.05)
     x, y = cyd_telearn.touches()
 
-    if x == 0 and y == 0:
+    if x == 0 and y == 0: # 若螢幕沒有被「觸控」，x 及 y 都會是 0, 不做任何事
         continue
 
     x, y = cyd_telearn.touch_to_display(x, y)
@@ -36,11 +36,10 @@ while True:
     x = min(max(x, r + 1), cyd_telearn.width - (r + 1))
 
     # Create circle
-    # cyd_telearn.fill_circle(x, y, r, colors[c])
+    cyd_telearn.fill_circle(x, y, r, colors[c])
     cyd_telearn.fill_rectangle(x, y, 10, 10, colors[c])
-    c = (c + 1) % len(colors)
     
+    c = (c + 1) % len(colors) # index 是 0,1,2，除以多少會是 0,1,2 ?
     # 隨機抽取所有顏色
     # cyd_telearn.fill_circle(x, y, r, getRandomColor())
-
 cyd_telearn.shutdown()
