d2bc1028fbb6b9bf02afc9cf2ebc95aa038ffc80
Roger Lo <sobadrush@icloud.com>
2024-06-28 21:59:36 +0800

    繪製選單

--- main_cola.py
+++ main_cola.py
@@ -6,3 +6,14 @@ cyd_telearn = CYD_Telearn()
 cyd_telearn.clear_screen()
 
 ###########################################
+
+# 定義按鈕文本
+button_texts = ["Circle", "Star", "Exit"]
+
+# 顯示按鈕
+y = 40
+cyd_telearn.display_text("Please choose a shape:", 10, 20, cyd_telearn.colors["YELLOW"])
+for text in button_texts:
+    cyd_telearn.fill_rectangle(10, y, 220, 30, cyd_telearn.colors["BLUE"])
+    cyd_telearn.display_text(text, 15, y + 10, cyd_telearn.colors["WHITE"])
+    y += 40
